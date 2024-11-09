22161f37e7d8b98b9a734c1a08fe3a94d593895f
Hirwa  Alain Fabrice <alainfabricehirwa@gmail.com>
2024-11-09 13:14:23 +0200

    Create codewarskata.js

---
+++ codewarskata.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3, 4, 5];
+const newNumber = numbers.map(num=> num*2);
+console.log(newNumber);
+
+//even numbers 
+const numberss = [1, 2, 3, 4, 5];
+const newNumberr=numberss.filter(num=>num%2==0);
+console.log(newNumberr);
