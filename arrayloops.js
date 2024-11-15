ca0bbc1414c9e33df011681aee9917f627cae4eb
Hirwa  Alain Fabrice <alainfabricehirwa@gmail.com>
2024-11-16 00:27:39 +0200

    arrayloops.js

--- arrayloops.js
+++ arrayloops.js
@@ -37,7 +37,7 @@ const numbers = [4,6,7,8,9,12];
   // to loop over the following array and add 100 to each of the values if the value is divisible by 3.
 
 
-  var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4, 19, 300, 3775, 299, 36, 209, 148, 169, 299, 6, 109, 20, 58, 139, 59, 3, 1, 139];
+  var test = [12, 929, 11, 3, 199, 1000, 1, 24, 37, 4, 19, 300, 3775, 299, 36, 209, 148, 169, 299, 6, 109, 20, 58, 139, 59, 3, 1, 139];
    test.forEach((num , index)=>{
        if (num%3==0) {
         test[index]+=100;
