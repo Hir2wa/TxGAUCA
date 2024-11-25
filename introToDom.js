7c8a0af127051afb3a990a68303ef351e52e26e0
Alain <alainfabricehirwa@gmail.com>
2024-11-25 12:01:17 +0200

    new commit

---
+++ introToDom.js
@@ -0,0 +1,16 @@
+//  DOM is  a web api     
+//  here  i will  the DOM
+// modify page   content 
+//browser events
+//performance code 
+//
+// The DOM is standardized by the W3C. There are a number of specifications that make up the DOM, here are few:
+
+// Core Specification
+// Events Specification
+// Style Specification
+// Validation Specification
+// Load and Save Specification
+// 
+// we  can select an element by id
+document.getElementById('footer')
