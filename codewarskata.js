81edfe2dffdb6c3530f0a645c2685c1271242edb
Alain <alainfabricehirwa@gmail.com>
2024-11-29 15:03:43 +0200

    new commit

--- codewarskata.js
+++ codewarskata.js
@@ -329,7 +329,7 @@ const iterator = arwray[Symbol.iterator]();
             
         
 
-              // Return an array
+              //FizzBuzz-FizzBuzz
 function fizzbuzz(n)
 {
    let myArray = [];
@@ -353,4 +353,21 @@ console.log(fizzbuzz(20));
  
 
 
-            
+function fizzbuzz(n) {
+  let result = [];
+  
+  for (let i = 1; i <= n; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+          result.push("FizzBuzz");
+      } else if (i % 3 === 0) {
+          result.push("Fizz");
+      } else if (i % 5 === 0) {
+          result.push("Buzz");
+      } else {
+          result.push(i);
+      }
+  }
+
+  return result;
+}
+
