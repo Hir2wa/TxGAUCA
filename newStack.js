7eb07c683191b4d0e4ff098b05b7e48c9bbfa815
Alain <alainfabricehirwa@gmail.com>
2024-12-03 16:31:40 +0200

    new commit

---
+++ newStack.js
@@ -0,0 +1,13 @@
+function first() {
+    console.log('First function');
+    second();
+    console.log('First function end');
+  }
+  
+  function second() {
+    console.log('Second function');
+  }
+  
+  first();
+  console.log('Program end');
+  
