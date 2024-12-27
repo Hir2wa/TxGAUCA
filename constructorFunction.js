e19937804fc441cc9bfb404f2c277668a3b2990b
Hir2wa <alainfabricehirwa@gmail.com>
2024-12-28 00:38:58 +0200

    ..

--- constructorFunction.js
+++ constructorFunction.js
@@ -16,4 +16,17 @@ const person2 = new Person('Bob');
 const boundGreet = person1.greet.bind(person2);
 boundGreet(); // Output: Hello, my name is Bob
 
+  // we  go on curry function 
+  // A curried function to add three numbers
+function add(a) {
+    return function(b) {
+      return function(c) {
+        return a + b + c;
+      };
+    };
+  }
+  
+  // Using the curried function
+  const result = add(2)(3)(4);
+  console.log(result);  // Output: 9
   
