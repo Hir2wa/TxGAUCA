bbaaad688ae4ce390fc3d20d0d28aa95e626f110
Hir2wa <alainfabricehirwa@gmail.com>
2024-12-28 00:11:03 +0200

    ..

---
+++ objectDestructuirng.js
@@ -0,0 +1,6 @@
+//object  destructuring is a feature in js  that allows you  to  ecxtrat properties of  object into variable  
+let obj1 ={
+    name : "alain",
+    age: 12
+}
+let { name,age}=obj1;
