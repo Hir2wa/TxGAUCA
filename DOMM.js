89d087c8cbc5d924f5bbf8d8dfa250b60adb4146
Hir2wa <alainfabricehirwa@gmail.com>
2024-12-29 22:08:25 +0200

    ...

---
+++ DOMM.js
@@ -0,0 +1,3 @@
+// BOM is a set of features provided by the browser that acts as
+//  a workspace for JavaScript to control and interact with the browser environment
+//  and external functionalities like navigation, screen dimensions, and popups.
