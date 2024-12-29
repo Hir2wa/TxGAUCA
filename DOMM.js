0cb6a550ac799b00b453fdd465735f4c348e91ed
Hir2wa <alainfabricehirwa@gmail.com>
2024-12-29 23:14:52 +0200

    ...

--- DOMM.js
+++ DOMM.js
@@ -1,3 +1,6 @@
 // BOM is a set of features provided by the browser that acts as
 //  a workspace for JavaScript to control and interact with the browser environment
 //  and external functionalities like navigation, screen dimensions, and popups.
+
+// firstchild  //  first element  child    these  are  difference since  it can return also text  it is  available
+// previdouselementsibling
